2cde9ec691e2a57164fc071827050d704b87aab4
me <codercoderme@gmail.com>
2020-04-17 21:02:12 +0800

    add viewmodel
    ViewModel is a simple data class with no behavior, it’s responsible for representing the current view state.

---
+++ Mobile/ViewModel/MasterViewModel.swift
@@ -0,0 +1,17 @@
+//
+//  MasterViewViewModel.swift
+//  Mobile
+//
+//  Created by jack on 17/4/20.
+//  Copyright © 2020 fl. All rights reserved.
+//
+
+import Foundation
+
+
+struct MasterViewModel {
+    
+    
+    
+    
+}
