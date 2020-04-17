07c0f26b12f58524e32294c3982dea0b65cd29b1
me <codercoderme@gmail.com>
2020-04-17 18:52:01 +0800

    add network layer

---
+++ Mobile/Networking/WebService.swift
@@ -0,0 +1,32 @@
+//
+//  WebService.swift
+//  Mobile
+//
+//  Created by jack on 17/4/20.
+//  Copyright © 2020 fl. All rights reserved.
+//
+
+import Foundation
+
+
+protocol WebServiceProtocol {
+    
+    
+}
+
+
+
+class WebService {
+    
+    
+    
+    
+    
+}
+
+
+
+
+extension WebService: WebServiceProtocol {
+    
+}
